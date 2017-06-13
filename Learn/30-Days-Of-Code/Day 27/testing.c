37ca7b674fa8b626e2d7ffa3f26e5dfe23321d50
Adriel Martinez <adriel@adrielm.ca>
2017-06-13 14:41:09 -0400

    Day 27

---
+++ Learn/30-Days-Of-Code/Day 27/testing.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("5\n");
+    printf("5 3\n-1 0 1 10 9\n");
+    printf("5 2\n-1 0 1 10 9\n");
+    printf("5 3\n-1 0 1 10 9\n");
+    printf("5 2\n-1 0 1 10 9\n");
+    printf("5 3\n-1 0 1 10 9\n");
+    return 0;
+}
---
+++ Learn/30-Days-Of-Code/Day 27/testing.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    cout << "5" << endl;
+    cout << "5 3" << endl << "-1 0 1 10 9" << endl;
+    cout << "5 2" << endl << "-1 0 1 10 9" << endl;
+    cout << "5 3" << endl << "-1 0 1 10 9" << endl;
+    cout << "5 2" << endl << "-1 0 1 10 9" << endl;
+    cout << "5 3" << endl << "-1 0 1 10 9" << endl;
+    return 0;
+}
